6da61b60fe6786086a90e5e0cafb061bacd38b73
Sunil Green <green380@purdue.edu>
2020-02-14 12:18:33 -0500

    Established Connection to MongoDB Backend

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,34 @@ app.use('/api/contact-form', contactRouter);
 app.use('/api/content-form', contentRouter);
 */
 
+/* Database connection code */
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = "mongodb+srv://Sunil:icKxYzHJ1chT3M1H@cluster0-94hkc.gcp.mongodb.net/test?retryWrites=true&w=majority"
+const dbName = "BetterBNB";
+
+//Create a new MongoClient
+const client = new  MongoClient(url);
+
+client.connect(function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+  
+    const db = client.db(dbName);
+  
+    const col = db.collection('Users').find({}).toArray(function(err, result){
+        if (err) throw err;
+        console.log(result);
+        //db.close();   
+
+    });
+    
+  });
+
+
+
+
+
 if (process.env.NODE_ENV === "production") {
     app.use(express.static('client/build'));
     app.use('*', express.static('client/build'));
@@ -25,6 +53,7 @@ if (process.env.NODE_ENV === "production") {
         res.sendFile(path.resolve(__dirname,"client", "build", "index.html"))
     });
 }
+
 
 app.listen(port, () => { 
     console.log(`Server is running on port: ${port}`);
