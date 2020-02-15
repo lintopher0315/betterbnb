3a3ebb3c421a31554edc4cc14aa04a18b0aa00dd
uniqueuser-repo <57453461+uniqueuser-repo@users.noreply.github.com>
2020-02-15 00:57:24 -0500

    Add unit tests to extract_location_details.py

--- backend/extract_location_details.py
+++ backend/extract_location_details.py
@@ -1,6 +1,7 @@
 from uszipcode import SearchEngine, SimpleZipcode
 import requests
 import json
+import unittest
 
 def extract_zipcode_with_url(website_url):
 
@@ -35,7 +36,7 @@ def extract_zipcode_with_lat_lng(lat, lng):
     # latitude and longitude
 
     search = SearchEngine()
-    list_of_zipcodes = search.by_coordinates(listing_latitude, listing_longitude, 2)
+    list_of_zipcodes = search.by_coordinates(listing_latitude, listing_longitude, 5)
 
     # retrieve first zipcode found
     first_zipcode = list_of_zipcodes[0].zipcode
@@ -56,3 +57,45 @@ def extract_city_and_state_with_lat_lng(lat, lng):
     first_zipcode = list_of_zipcodes[0]
 
     return first_zipcode.city, first_zipcode.state
+
+
+# UNIT TEST CLASSES BELOW #
+# UNIT TEST CLASSES BELOW #
+# UNIT TEST CLASSES BELOW #
+# UNIT TEST CLASSES BELOW #
+
+class TestZipcodeExtraction(unittest.TestCase):
+
+    def test_ZipBatchOne(self):
+
+        # LEFT COLUMN IS THE "EXPECTED" RESULT #
+
+        self.assertEqual(str(60007), extract_zipcode_with_lat_lng(42.01, -87.99))
+        self.assertEqual(str(47906), extract_zipcode_with_lat_lng(40.49, -86.96))
+        self.assertEqual(str(23801), extract_zipcode_with_lat_lng(37.24, -77.34))
+        self.assertEqual(str(33815), extract_zipcode_with_lat_lng(28.09, -82.02))
+        self.assertEqual(str(35801), extract_zipcode_with_lat_lng(34.74, -86.55))
+        self.assertEqual(str(35805), extract_zipcode_with_lat_lng(34.69, -86.61))
+        self.assertEqual(str(85034), extract_zipcode_with_lat_lng(33.44, -112.02))
+        self.assertEqual(str(80211), extract_zipcode_with_lat_lng(39.77, -105.01))
+        self.assertEqual(str(60612), extract_zipcode_with_lat_lng(41.88, -87.69))
+
+    def test_ZipBatchTwo(self):
+
+        # LEFT COLUMN IS THE "EXPECTED" RESULT #
+
+        city, state = extract_city_and_state_with_lat_lng(42.03, -87.88)
+        self.assertListEqual(["Des Plaines", "IL"], [city, state])
+
+        city, state = extract_city_and_state_with_lat_lng(37.77, -122.42)
+        self.assertListEqual(["San Francisco", "CA"], [city, state])
+
+        city, state = extract_city_and_state_with_lat_lng(38.91, -77.04)
+        self.assertListEqual(["Washington", "DC"], [city, state])
+
+        city, state = extract_city_and_state_with_lat_lng(33.44, -112.02)
+        self.assertListEqual(["Phoenix", "AZ"], [city, state])
+
+        city, state = extract_city_and_state_with_lat_lng(39.77, -105.01)
+        self.assertListEqual(["Denver", "CO"], [city, state])
+
