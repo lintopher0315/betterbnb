b43981b73efe6128074f6c7874704c8f1390bba4
lintopher0315 <lin978@purdue.edu>
2020-02-15 15:42:48 -0500

    remade results cards and added more info

--- client/src/components/ListingCard.js
+++ client/src/components/ListingCard.js
@@ -1,30 +1,102 @@
 import React from 'react';
+import cx from 'clsx';
+import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
+import { makeStyles } from '@material-ui/styles';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Rating from '@material-ui/lab/Rating';
+import IconButton from '@material-ui/core/IconButton';
+import LocationOn from '@material-ui/icons/LocationOn';
+import MoreHoriz from '@material-ui/icons/MoreHoriz';
+import Favorite from '@material-ui/icons/Favorite';
+import FaceGroup from '@mui-treasury/components/group/face';
+import { useWideCardMediaStyles } from '@mui-treasury/styles/cardMedia/wide';
+import { useFadedShadowStyles } from '@mui-treasury/styles/shadow/faded';
+import { usePushingGutterStyles } from '@mui-treasury/styles/gutter/pushing';
+
+const useStyles = makeStyles(() => ({
+    content: {
+        padding: 24,
+        margin: '-24% 16px 0',
+        backgroundColor: '#fff',
+        borderRadius: 4,
+        position: 'relative',
+        opacity: '95%',
+    }
+}))
 
 function ListingCard(props) {
+    const styles = useStyles();
+    const mediaStyles = useWideCardMediaStyles();
+    const shadowStyles = useFadedShadowStyles();
+    const gutterStyles = usePushingGutterStyles({ firstExcluded: true });
     return (
-        <Card id="listing-card">
-            <CardActionArea>
-                <CardMedia
-                    id="listing-card-media" 
-                    image={props.img}
-                    title={props.title}
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {props.title}
+        <Card elevation={0} className="listing-card-root">
+            <CardMedia
+                classes={mediaStyles}
+                image={
+                    props.img
+                }
+            />
+            <CardContent className={cx(shadowStyles.root, styles.content)}>
+                <IconButton className="listing-card-fav">
+                    <Favorite />
+                </IconButton>
+                <h3 className="listing-card-title">{props.title}</h3>
+                <Box color={'grey.500'} display={'flex'} alignItems={'center'} mb={1}>
+                    <LocationOn className="listing-card-loc" />
+                    <span>{props.loc}</span>
+                </Box>
+                <Box
+                    display={'flex'}
+                    alignItems={'center'}
+                    mb={1}
+                    className={gutterStyles.parent}
+                >
+                    <Rating id="listing-card-star" name={'rating'} value={props.rating} size={'small'} />
+                    <Typography variant={'body2'} className="listing-card-rate">
+                        {(props.rating).toFixed(1)}
                     </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {props.text}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
+                </Box>
+                <Typography id="listing-card-text" color={'textSecondary'}>
+                    {props.price}
+                </Typography>
+                <Box
+                    mt={2}
+                    display={'flex'}
+                    justifyContent={'space-between'}
+                    alignItems={'center'}
+                >
+                    <Box
+                        display={'flex'}
+                        alignItems={'center'}
+                        className={gutterStyles.parent}
+                    >
+                        <FaceGroup
+                            faces={[
+                                'https://i.pravatar.cc/300?img=1',
+                                'https://i.pravatar.cc/300?img=2',
+                                'https://i.pravatar.cc/300?img=3',
+                                'https://i.pravatar.cc/300?img=4',
+                            ]}
+                            size={32}
+                            offset={-12}
+                        />
+                        <Typography
+                            component={'span'}
+                            variant={'body2'}
+                            color={'textSecondary'}
+                        >
+                            +420
+                        </Typography>
+                    </Box>
+                    <IconButton className="listing-card-opt" size={'small'}>
+                        <MoreHoriz />
+                    </IconButton>
+                </Box>
+            </CardContent>
         </Card>
     )
 }
--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -26,60 +26,76 @@ function SearchResults() {
                 <Row id="results-row">
                     <Col>
                         <ListingCard
-                            img="https://atlantis.nyc3.digitaloceanspaces.com/styled/72025f140f22a3eb32950bbb9d76e68d"
-                            title="Beach"
-                            text="This is a beach, which has sand and waves. Despite what people might think, beaches are NOT fun."
+                            img="https://cdn.cnn.com/cnnnext/dam/assets/160506135321-hong-kong-hotel-park-lane-super-169.jpg"
+                            title="Beautiful Sea Apartment"
+                            loc="Singapore"
+                            rating={4.0}
+                            price="$26 daily rate"
                         />
                     </Col>
                     <Col>
                         <ListingCard
-                            img="https://news.uns.purdue.edu/images/+2007/jischkedrive-signs.jpg"
-                            title="205 South Martin Jischke Drive"
-                            text="B^)"
+                            img="https://cdn.cnn.com/cnnnext/dam/assets/160506135313-hong-kong-hotel-peninsula-super-169.jpg"
+                            title="Hong Kong Peninsula Hotel"
+                            loc="Hong Kong"
+                            rating={3.0}
+                            price="$32 daily rate"
                         />
                     </Col>
                     <Col>
                         <ListingCard
-                            img="https://static.hltv.org/images/playerprofile/thumb/11227/800.jpeg?v=3"
-                            title="Randy"
-                            text="Big"
+                            img="https://www.elitetraveler.com/wp-content/uploads/2013/06/night-view4.jpg"
+                            title="Mandarin Oriental"
+                            loc="San Francisco"
+                            rating={3.0}
+                            price="$43 daily rate"
                         />
                     </Col>
                     <Col>
                         <ListingCard
-                            img="https://static.hltv.org/images/playerprofile/thumb/11227/800.jpeg?v=3"
-                            title="Randy"
-                            text="Randy"
+                            img="https://travel.home.sndimg.com/content/dam/images/travel/fullset/2013/03/28/2d/best-hotel-views_ss_002.rend.hgtvcom.966.725.suffix/1491592688614.jpeg"
+                            title="Four Seasons Hotel"
+                            loc="Sydney"
+                            rating={5.0}
+                            price="$65 daily rate"
                         />
                     </Col>
                 </Row>
                 <Row id="results-row">
                     <Col>
                         <ListingCard
-                            img="https://static.hltv.org/images/playerprofile/thumb/11227/800.jpeg?v=3"
-                            title="Randy"
-                            text="Is"
+                            img="https://www.elitetraveler.com/wp-content/uploads/2013/06/Royal-Penthouse-Twilight-Terrace-Corinthia-Hotel-London.jpg"
+                            title="Corinthia Hotel"
+                            loc="England"
+                            rating={4.0}
+                            price="$44 daily rate"
                         />
                     </Col>
                     <Col>
                         <ListingCard
-                            img="https://static.hltv.org/images/playerprofile/thumb/11227/800.jpeg?v=3"
-                            title="Randy"
-                            text="Watching"
+                            img="https://www.elitetraveler.com/wp-content/uploads/2013/06/Jade-Mountain-St-resized.jpg"
+                            title="Jade Mountain"
+                            loc="St. Lucia"
+                            rating={3.0}
+                            price="$123 daily rate"
                         />
                     </Col>
                     <Col>
                         <ListingCard
-                            img="https://static.hltv.org/images/playerprofile/thumb/11227/800.jpeg?v=3"
-                            title="Randy"
-                            text="You"
+                            img="https://www.elitetraveler.com/wp-content/uploads/2013/06/new_RCSINGA_00146.jpg"
+                            title="Ritz-Carlton"
+                            loc="Singapore"
+                            rating={3.5}
+                            price="$84 daily rate"
                         />
                     </Col>
                     <Col>
                         <ListingCard
-                            img="https://static.hltv.org/images/playerprofile/thumb/11227/800.jpeg?v=3"
-                            title="Randy"
-                            text="Sleep"
+                            img="https://dynaimage.cdn.cnn.com/cnn/q_auto,w_412,c_fill,g_auto,h_232,ar_16:9/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F160506140647-hong-kong-ritz-carlton.jpg"
+                            title="InterContinental Hong Kong"
+                            loc="Hong Kong"
+                            rating={4.5}
+                            price="$66 daily rate"
                         />
                     </Col>
                 </Row>
