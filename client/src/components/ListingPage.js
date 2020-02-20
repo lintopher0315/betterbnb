60348a85073111894ef75c9a7ec175dc690367f9
lintopher0315 <lin978@purdue.edu>
2020-02-20 15:49:13 -0500

    added sample listing description page; includes image carousel, description & rating, and interactive map

--- client/src/components/ListingPage.js
+++ client/src/components/ListingPage.js
@@ -1,7 +1,95 @@
 import React from 'react';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { Container, Col, Row } from 'react-bootstrap';
+import Rating from '@material-ui/lab/Rating';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function ListingPage() {
 
+    var settings = {
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '400px',
+        autoplay: true,
+        pauseOnHover: true,
+    }
+
+    const position = [-33.861691, 151.207687]
+
+    return (
+        <div>
+            <div id="carousel-container">
+                <Slider {...settings}>
+                    <div>
+                        <img src="https://picsum.photos/id/1018/1000/600/" alt="" />
+                    </div>
+                    <div>
+                        <img src="https://picsum.photos/id/1015/1000/600/" alt="" />
+                    </div>
+                    <div>
+                        <img src="https://picsum.photos/id/1019/1000/600/" alt="" />
+                    </div>
+                </Slider>
+            </div>
+            <Container fluid={true}>
+                <Row>
+                    <Col style={{paddingRight: '100px'}}>
+                        <div id="desc-col">
+                            <div id="listing-page-title">
+                                Four Seasons Hotel
+                            </div>
+                            <div id="listing-page-loc">
+                                Sydney
+                            </div>
+                            <Rating
+                                id="listing-page-star"
+                                name={'rating'}
+                                value={5}
+                                size={'medium'}
+                            />
+                            <div id="listing-desc-title">
+                                Description
+                            </div>
+                            <div id="listing-desc-text">
+                                Location doesn’t get any better than this: 
+                                Iconic Sydney Harbor sits at the doorstep of our 
+                                award-winning Central Business District Hotel, with historic 
+                                The Rocks and Circular Quay right next door. Head out to explore the nearby shopping, 
+                                outdoor adventure and vibrant social scene that surround us, 
+                                then meet back at Mode Kitchen & Bar for fresh, local cuisine and drinks at Grain. 
+                                In Sydney, work and play are never too far apart. And that’s just the way we like it.
+                            </div>
+                        </div>
+                    </Col>
+
+                    <Col style={{paddingLeft: '100px'}}>
+                        <div id="map-col">
+                            <div id="listing-map">
+                                <Map style={{width: '100%', height: '500px'}} center={position} zoom={13}>
+                                    <TileLayer
+                                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                    />
+                                    <Marker position={position}>
+                                        <Popup>
+                                            Your Location
+                                        </Popup>
+                                    </Marker>
+                                </Map>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
 }
 
 export default ListingPage;
--- client/src/components/WebRouter.js
+++ client/src/components/WebRouter.js
@@ -5,13 +5,14 @@ import Home from './Home.js';
 import Contact from './Contact.js'
 import Header from'./Header.js';
 import SearchResults from './SearchResults.js'
+import ListingPage from './ListingPage.js'
 
 function WebRouter() {
     return (
         <div>
             <Header />
             <Switch>
-                <Route exact path='/' component={() => <SearchResults />} />
+                <Route exact path='/' component={() => <ListingPage />} />
                 <Route path='/contact' component={() => <Contact />} />
             </Switch>
         </div>
