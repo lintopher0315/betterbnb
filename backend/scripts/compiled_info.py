1b1c079e5ee651d9b9fd3d84072614426d233dcc
uniqueuser-repo <57453461+uniqueuser-repo@users.noreply.github.com>
2020-02-26 12:44:25 -0500

    Patch merging dicts of different types of data

--- backend/scripts/compiled_info.py
+++ backend/scripts/compiled_info.py
@@ -8,13 +8,17 @@ executor = concurrent.futures.ThreadPoolExecutor()
 
 # initialize all return values in global scope
 crime_thread_obj = {}
+
+# initialize the compiled_dict which we will change to json pre-file write
+compiled_dict = {}
+
 # INITIALIZE GLOBAL THREAD OBJS FOR OTHER APIS UNDER crime_thread_obj ABOVE
 
 
 
 # if the only passed argument is an address
 if len(sys.argv) == 2:
-    # starts running the thread, stores in object to later get return value
+    # starts running the thread to get crime data, stores in object to later get return value
     crime_thread_obj = executor.submit(get_crime_data_with_address, sys.argv[1])
     # ADD SIMILAR CALL TO ABOVE FOR OTHER APIs HERE
 # otherwise there are 2 more passed arguments where the first is lat and the second is lng
@@ -32,8 +36,8 @@ else:
 write_crime_dict = crime_thread_obj.result()
 # ADD SIMILAR CALL TO ABOVE
 
-# WHEN OTHER APIs ARE ADDED, CHANGE THIS LINE TO dict(write_crime_dict.items() + newAPIDictName.items())
-compiled_dict = dict(write_crime_dict.items())
+compiled_dict['crime_data'] = write_crime_dict
+#compiled_dict['TYPE_OF_DATA'] = DATA_DICT <--------- THIS IS AN EXAMPLE. ADD THIS IF YOU'RE ADDING A NEW API.
 
 
 f = open("compiled_data.txt", "w")
