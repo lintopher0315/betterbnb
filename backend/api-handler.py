4ee65eb89fafc04cc7c8e972d796d35263864e21
Bradley Bottomlee <bradley.bttm@gmail.com>
2020-02-26 11:22:00 -0500

    Develop API endpoint

---
+++ backend/api-handler.py
@@ -0,0 +1,4 @@
+# this python script will be called to handle a request from a Node.js endpoint
+# the script will then return the necessary information to the Node.js endpoint 
+# the script employs the use of threads in order to serve clients in the fastest
+# possible way
