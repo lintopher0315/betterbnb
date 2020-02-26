a3b795ee8fecdcde8531ea5cec80272683c5c2f2
uniqueuser-repo <57453461+uniqueuser-repo@users.noreply.github.com>
2020-02-26 13:30:52 -0500

    Modify comment structure

--- backend/scripts/compiled_info.py
+++ backend/scripts/compiled_info.py
@@ -8,16 +8,25 @@ from FBI_Crime_Data import get_crime_data_with_lat_lng, get_crime_data_with_addr
 def compile_info_lat_long(lat, longt):
     # set up multithreading
     executor = concurrent.futures.ThreadPoolExecutor()
-    # initialize the compiled_dict which we will change to json pre-file write
+
+    # create object which will hold the return value of the threaded get_crime_data_with_lat_lng
     crime_thread_obj = executor.submit(get_crime_data_with_lat_lng, lat, longt)
+    # NEW DATA SOURCES: add data_source_obj above that does the same thing
 
     generate_report(crime_thread_obj)
+    # NEW DATA SOURCES: add another argument above and then modify the parameters of generate_report below
+
 
 def compile_info_addr(addr):
+    # set up multithreading
     executor = concurrent.futures.ThreadPoolExecutor()
+
+    # create object which will hold the return value of get_crime_data_with_lat_lng
     crime_thread_obj = executor.submit(get_crime_data_with_address, addr)
+    # NEW DATA SOURCES: add data_source_obj above that does the same thing
 
     generate_report(crime_thread_obj)
+    # NEW DATA SOURCES: add another argument above and then modify the parameters of generate_report below
 
 
 def generate_report(crime_thread_obj):
@@ -25,11 +34,11 @@ def generate_report(crime_thread_obj):
         system("rm compiled_data.txt")
 
     write_crime_dict = crime_thread_obj.result()
-    # ADD SIMILAR CALL TO ABOVE
-    #compiled_dict['TYPE_OF_DATA'] = DATA_DICT <--------- THIS IS AN EXAMPLE. ADD THIS IF YOU'RE ADDING A NEW API.
+    # NEW DATA SOURCES: ADD SIMILAR CALL TO ABOVE
     
     compiled_dict = {}
     compiled_dict['crime_data'] = write_crime_dict
+    # NEW DATA SOURCES: compiled_dict['TYPE_OF_DATA'] = DATA_DICT <--------- THIS IS AN EXAMPLE. ADD THIS IF YOU'RE ADDING A NEW API.
     
     f = open("compiled_data.txt", "w")
     json.dump(compiled_dict, f)
