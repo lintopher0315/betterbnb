b91fd36640626f817314a2cd941f2814debe1e7c
uniqueuser-repo <57453461+uniqueuser-repo@users.noreply.github.com>
2020-02-27 04:16:25 -0500

    Re-introduce uszipcode. Add population key to data dict in compiled_info.py.

---
+++ backend/scripts/population_data.py
@@ -0,0 +1,14 @@
+from uszipcode import SearchEngine
+from extract_location_details import extract_zipcode_with_lat_lng, extract_zipcode_with_address
+
+def get_population_data_with_lat_lng(lat, lng):
+    zipcode = extract_zipcode_with_lat_lng(lat, lng)
+    search = SearchEngine(simple_zipcode=False)
+    zipcodeDetails = search.by_zipcode(str(zipcode))
+    return zipcodeDetails.population
+
+def get_population_data_with_address(address):
+    zipcode = extract_zipcode_with_address(address)
+    search = SearchEngine(simple_zipcode=False)
+    zipcodeDetails = search.by_zipcode(str(zipcode))
+    return zipcodeDetails.population
