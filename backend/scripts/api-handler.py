3a1a9af5e0948e96b9b60bd0bf3649e169565e88
Bradley Bottomlee <bradley.bttm@gmail.com>
2020-02-27 23:03:17 -0500

    Implement dynamic content on chrome extension

--- backend/scripts/api-handler.py
+++ backend/scripts/api-handler.py
@@ -8,18 +8,18 @@ from compiled_info import compile_info_lat_long
 # possible way
 
 if __name__ == "__main__":
-    try:
-        url = sys.argv[1] 
-        latlong = list(extract_lat_lng_with_url(url))
-        compile_info_lat_long(latlong[0], latlong[1])
+    url = sys.argv[1] 
+    latlong = list(extract_lat_lng_with_url(url))
+    compile_info_lat_long(latlong[0], latlong[1])
 
-        somefile = open("compiled_data.txt", "r")
-        for line in somefile:
-            print(line)
-            sys.stdout.flush()
-        somefile.close() 
+    somefile = open("compiled_data.txt", "r")
+    for line in somefile:
+        print(line)
+        sys.stdout.flush()
+    somefile.close() 
 
-
+'''
     except Exception as e:
-        print(e)
+        print(e.)
         print("error")
+'''
