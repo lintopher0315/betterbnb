26a296b737176c0f39df7390a290e8aca6430086
Bradley Bottomlee <bradley.bttm@gmail.com>
2020-03-07 19:03:41 -0500

    Create UI for Settings panel on chrome extension

--- chrome-extension/src/components/settings.component.js
+++ chrome-extension/src/components/settings.component.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react'
-import { Spinner } from 'react-bootstrap'
+import { Spinner, Form } from 'react-bootstrap'
 
 export default class Settings extends Component {
-    
+    constructor(props) {
+        super(props)
+
+        // do some stuff here related to the settings
+        this.state = {
+            currentSelection: 0 
+        }
+    }
+
+
     
     render() {
         return(
-            <div className='container' style={{paddingTop: "35%"}}>
+            <div className='container' style={{paddingTop: "2%"}}>
                 <div className='text-center'>
-                    <Spinner animation="grow" variant="dark" />
                     <h1>Settings</h1>
+                    <p>Check your desired options (3 allowed)</p>
+                    <hr />
+                    <p align="justify">
+                        <Form.Check 
+                            type="switch"
+                            id="crime"
+                            label="Crime"
+                        /> <br />
+                        <Form.Check 
+                            type="switch"
+                            id="population"
+                            label="Population"
+                        /> <br /> 
+                        <Form.Check 
+                            type="switch"
+                            id="restaurants"
+                            label="Restaurants"
+                        />
+                    </p>
+                    <br />
+                    <p><b>Coming Soon:</b> Sign-in with BetterBNB</p>
                 </div>
             </div>
         )
