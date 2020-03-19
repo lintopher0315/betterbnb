e8c5ad0df7c86c13e439f204773584f9fca33c2e
uniqueuser-repo <57453461+uniqueuser-repo@users.noreply.github.com>
2020-03-18 19:42:21 -0500

    Update unit tests to have a smaller radius; add sleep time (API is returning varying results?? maybe rate limiting)

--- backend/scripts/lodging.py
+++ backend/scripts/lodging.py
@@ -1,9 +1,10 @@
 from extract_location_details import extract_lat_lng_with_address
 import requests
 import unittest
+import time
 
 FIRST_HALF_QUERY = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="
-SECOND_HALF_QUERY = "&radius=1500&keyword=lodging&key="
+SECOND_HALF_QUERY = "&radius=500&keyword=lodging&key="
 CONSTANT_GOOGLE_API_KEY = "AIzaSyACEj7IvA9oyKaApQikJKvSVm1B_nmFSUw"
 
 def get_lodging_data_with_lat_lng(lat, lng):
@@ -23,10 +24,13 @@ def get_lodging_data_with_address(address):
 class TestLodging(unittest.TestCase):
 
     def testLodgingData(self): # Not flexible; tests will break when new lodging becomes available; if failed, must also check that there isn't new lodging
-        # Test with anecdotally KNOWN locations; find lodgings within 1500m of the given latitude/longitude
-        self.assertEqual(4, len(get_lodging_data_with_lat_lng(40.4259, -86.9081)['results'])) # West Lafayette
+        # Test with anecdotally KNOWN locations; find lodgings within 500m of the given latitude/longitude
+        self.assertEqual(3, len(get_lodging_data_with_lat_lng(40.4259, -86.9081)['results'])) # West Lafayette
+        time.sleep(0.25) # sleep in an attempt to avoid getting results limited
         self.assertEqual(0, len(get_lodging_data_with_lat_lng(42.0778, -87.8223)['results'])) # Glenview
-        self.assertEqual(1, len(get_lodging_data_with_lat_lng(42.0334, -87.8834)['results'])) # Des Plaines
-        self.assertEqual(7, len(get_lodging_data_with_lat_lng(30.2241, -92.0198)['results'])) # Lafayette
+        time.sleep(0.25) # sleep in an attempt to avoid getting results limited
+        self.assertEqual(0, len(get_lodging_data_with_lat_lng(42.0334, -87.8834)['results'])) # Des Plaines
+        time.sleep(0.25) # sleep in an attempt to avoid getting results limited
+        self.assertEqual(5, len(get_lodging_data_with_lat_lng(30.2241, -92.0198)['results'])) # Lafayette
 
 # Supplementary (manual, extra thorough): Verify specific data to make sure that the CORRECT lodgings are being returned; not just the number.
