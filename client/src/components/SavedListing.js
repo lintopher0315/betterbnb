35836b0edc7e2bdacc81a133646cbdd0e288f4ca
lintopher0315 <lin978@purdue.edu>
2020-03-26 20:55:43 -0400

    created ui for saved listings with left column scrollbar and map

--- client/src/components/SavedListing.js
+++ client/src/components/SavedListing.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Rating from '@material-ui/lab/Rating';
+import 'react-perfect-scrollbar/dist/css/styles.css';
 
 export class SavedListing extends Component {
 
@@ -8,15 +13,14 @@ export class SavedListing extends Component {
         this.state = {
             id: "5e793b4e25eca81d644b12a5",
             urls: [],
-            data: null
+            data: null,
+            map_loc: [10, 10],
         }
         
     }
       
 
     componentDidMount() {
-
-        
         const requestObj = {
             id: this.state.id,
         }
@@ -39,6 +43,7 @@ export class SavedListing extends Component {
                         axios.get('http://localhost:5000/api/report', {headers: {"url": this.state.urls[i]}})
                             .then(response => {
                                 this.setState({data: response.data})
+                                console.log(response.data)
                             })
                             .catch(err => {
                                 console.log(err)
@@ -53,23 +58,63 @@ export class SavedListing extends Component {
                 console.log(err)
                 this.setState({redirection: true})
             })
-        
-        
-
-        
     }
 
-   
+    changeMap() {
+        this.setState({map_loc: [-(Math.random() * 100 - 50), -(Math.random() * 100 - 50)]})
+    }
 
     render() {
-        return (
-        <div className='text-center'>
-            <h1>Saved Listings</h1>
-            <hr></hr>
-           
-                    
 
-        </div>
+        let temp = [];
+        for (let i = 0; i < 20; i++) {
+            temp.push(
+            <div>
+                <div  id="saved-listing-btn" onClick={this.changeMap.bind(this)}>
+                    <div id="saved-listing-title">
+                        Corinthia Hotel
+                    </div>
+                    <Rating
+                        name={'rating'}
+                        value={5}
+                        size={'medium'}
+                    />
+                </div>
+                <hr />
+            </div>)
+        }
+
+
+        return (
+            <div style={{paddingTop: '100px'}}> 
+                <Container fluid={true}>
+                    <Row>
+                        <Col md={4}>
+                            <PerfectScrollbar style={{height: '830px'}}>
+                                <div>
+                                    {temp}
+                                </div>
+                            </PerfectScrollbar>
+                        </Col>
+
+                        <Col md={8}>
+                            <div>
+                                <Map style={{width: '100%', height: '830px', zIndex: 1}} center={this.state.map_loc} zoom={13}>
+                                    <TileLayer
+                                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                    />
+                                    <Marker position={this.state.map_loc}>
+                                        <Popup>
+                                            Your Location
+                                        </Popup>
+                                    </Marker>
+                                </Map>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
       
         )
     }
