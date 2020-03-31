d0d9ccce8311c134b278f3984fabacf2c156459a
Bradley Bottomlee <bradley.bttm@gmail.com>
2020-03-31 00:48:07 -0500

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,9 @@
 import os
 if __name__ == "__main__":
     os.system("killall node")
-    os.chdir("client/")
+    os.chdir("backend/scripts")
+    os.system("source reqs/bin/activate")
+    os.chdir("../../client/")
     os.system("npm start &")
     os.chdir("../backend")
     os.system("npm start")
