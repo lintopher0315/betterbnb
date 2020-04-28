c1c4db7c7540707e024f46c8c758d96b2d6fd368
Anudeep Yakkala <ayakkala@purdue.edu>
2020-04-27 17:43:13 -0700

    Added a funciton to scrape title from a listing

--- backend/scripts/extract_location_details.py
+++ backend/scripts/extract_location_details.py
@@ -140,6 +140,28 @@ def extract_description(website_url):
     # return the description
     return listing_json['pdp_listing_detail']['sectioned_description']['description']
 
+def extract_title(website_url):
+    # upper case variables are constants
+    # 'r' means raw, doesn't worry about escape characters etc
+
+    FIRST_HALF_URL = r'https://www.airbnb.com/api/v2/pdp_listing_details/'
+    SECOND_HALF_URL = r'?_format=for_rooms_show&_p3_impression_id=p3_1579738698_5K3M9yAF%2FiR%2BU%2BHj&key=d306zoyjsyarp7ifhu67rjxn52tv0t20&'
+
+    # ONLY airbnb right now
+    # in the future define different methods with the
+    # different ways of retrieving the zipcode from the site
+
+    # strip the first 29 characters (get to beginning of listing id)
+    listing_id = website_url[29:]
+
+    # isolate the listing id
+    listing_id = listing_id[0:listing_id.find('?')]
+
+    # call the api, store the json in listing_json
+    listing_json = requests.get(FIRST_HALF_URL + listing_id + SECOND_HALF_URL).json()
+
+    # return the description
+    return listing_json['pdp_listing_detail']['sectioned_description']['name']
 
 
 def extract_city_and_state_with_lat_lng(lat, lng):
